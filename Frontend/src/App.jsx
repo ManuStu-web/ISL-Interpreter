f466ef751c8bb7f08b055855864d719161ccfe26
Manu Sharma <msharma.stu@gmail.com>
2026-03-23 20:40:35 +0530

    Inital Structure

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='h-screen w-full bg-gray-500 text-white'>
+      <h1 className='text-3xl'>
+        ISL Interpreter
+         </h1>
+    </div>
+  )
+}
+
+export default App
